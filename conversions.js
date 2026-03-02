272e1eba96e11cf1b911538f278a5e48236e06ce
Daksh Gour <dakshgour44@gmail.com>
2026-03-02 20:04:25 +0530

    Added conversions file

---
+++ conversions.js
@@ -0,0 +1,10 @@
+let score= "34";
+console.log(typeof score);
+console.log(typeof (score));
+
+let NumberInscore=Number(score);
+console.log(typeof NumberInscore);
+
+// "33" = 33
+// "33abc" == NotANumber
+//true = 1; false = 0
