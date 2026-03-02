cb283431166276c2b538106acff34d1e52452806
Daksh Gour <dakshgour44@gmail.com>
2026-03-02 20:04:18 +0530

    Added datatypes file

---
+++ datatypes.js
@@ -0,0 +1,5 @@
+let age = 34;
+let name = "DAKSH";
+//string , boolean , numbers, null (standalone value), undefined, -- these are primitive type of datatypes;
+// non primitive -- object 
+// null is type of object 
