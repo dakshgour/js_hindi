11916dacacb93cfa9fe40342e0ad809a948ffc8a
Daksh Gour <dakshgour44@gmail.com>
2026-03-02 20:03:06 +0530

    Added variables file

---
+++ variables.js
@@ -0,0 +1,8 @@
+const accountId = 60696
+let accountName="daksh"
+var accountPurpose="doomscrolling"
+accountregion="chandigarh"
+
+console.table([accountId,accountName,accountPurpose,accountregion]);
+
+// do not use var ever in Javascript ;
