a5a55addafbfc1a4ba91ef7f886a505c996b969f
Daksh Gour <dakshgour44@gmail.com>
2026-03-03 08:23:05 +0000

    new things to learn

--- conversions.js
+++ conversions.js
@@ -1,10 +1,42 @@
 let score= "34";
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
 
 let NumberInscore=Number(score);
-console.log(typeof NumberInscore);
+//console.log(typeof NumberInscore);
 
 // "33" = 33
 // "33abc" == NotANumber
 //true = 1; false = 0
+
+let str1="hello";
+let str2=" daksh";
+let str3=str1+str2;
+//console.log(str3);
+
+
+// problem kaha aegi Neeche dheko
+
+//console.log("2"+4);
+
+
+let a = 23;
+let b =a;
+b = 322;
+console.log(a);
+console.log(b);
+
+let user1={
+    name : "daksh",
+    class : "bds3",
+    age : 23
+};
+
+let user2=user1;
+user2.age=34;
+console.log(user1.age);
+console.log(user2.age);
+
+
+
+
